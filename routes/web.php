d01b37b27e00d903d8ee7accce3b748f0e6e1bd2
Jorge G <jorgegomezweb@gmail.com>
2022-04-11 13:40:59 +0200

    feat: adding home route

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,9 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', function() {
+    return Inertia::render('Welcome');
+})->name('home');
 
 Route::middleware([
     'auth:sanctum',
