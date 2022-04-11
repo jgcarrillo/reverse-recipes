f0f1106785b9383727d08d206cc6c1c0dc224f2a
Jorge G <jorgegomezweb@gmail.com>
2022-04-11 13:56:57 +0200

    feat: adding services component

---
+++ resources/js/Pages/Services.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="bg-gray-100 pb-8">
+        <section class="md:h-full flex items-center text-gray-800">
+            <div class="container mx-auto px-5 py-25 text-gray-800">
+                <div class="text-center mb-12">
+                    <h1 class="font-lora text-4xl md:text-5xl text-gray-800 font-bold pt-8">What we offer</h1>
+                </div>
+                <div class="flex flex-wrap -m-4">
+                    <div class="p-4 sm:w-1/2 lg:w-1/3">
+                        <div class="h-full border border-gray-200 rounded-lg overflow-hidden shadow-md">
+                            <img
+                                class="lg:h-72 md:h-48 w-full object-cover object-center"
+                                src="https://picsum.photos/id/1080/6858/4574"
+                                alt="blog"
+                            />
+                            <div class="p-6">
+                                <h1 class="font-lora text-2xl font-semibold mb-3">Fresh recipes</h1>
+                                <p class="font-monse leading-relaxed mb-3">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam modi, expedita quos doloremque autem ipsum
+                                    itaque incidunt ipsam reprehenderit fuga!
+                                    <code class="bg-yellow-400 px-1 py-0.5 rounded text-yellow-700 font-mono italic">Dolores</code> quisquam eius
+                                    cum accusamus?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-4 sm:w-1/2 lg:w-1/3">
+                        <div class="h-full border border-gray-200 rounded-lg overflow-hidden shadow-md">
+                            <img
+                                class="lg:h-72 md:h-48 w-full object-cover object-center"
+                                src="https://picsum.photos/id/292/3852/2556"
+                                alt="blog"
+                            />
+                            <div class="p-6">
+                                <h1 class="font-lora text-2xl font-semibold mb-3">Easy DIY</h1>
+                                <p class="leading-relaxed mb-3 font-monse">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    <code class="bg-yellow-400 px-1 py-0.5 rounded text-yellow-700 font-mono italic">adipisicing</code> elit.
+                                    Aperiam modi, expedita quos doloremque autem ipsum itaque incidunt ipsam reprehenderit fuga! Dolores quisquam
+                                    eius cum accusamus?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-4 sm:w-1/2 lg:w-1/3 grow">
+                        <div class="h-full border border-gray-200 rounded-lg overflow-hidden shadow-md">
+                            <img
+                                class="lg:h-72 md:h-48 w-full object-cover object-center"
+                                src="https://picsum.photos/id/312/3888/2592"
+                                alt="blog"
+                            />
+                            <div class="p-6">
+                                <h1 class="font-lora text-2xl font-semibold mb-3">24/7 Recipes</h1>
+                                <p class="font-monse leading-relaxed mb-3">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam modi, expedita quos doloremque
+                                    <code class="bg-yellow-400 px-1 py-0.5 rounded text-yellow-700 font-mono italic">autem</code> ipsum itaque
+                                    incidunt ipsam reprehenderit fuga! Dolores quisquam eius cum accusamus?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,6 +3,7 @@
 
     <Navbar />
     <Jumbo />
+    <Services />
 
 </template>
 
@@ -12,15 +13,17 @@
 <script>
 import { defineComponent } from 'vue'
 import { Head, Link } from '@inertiajs/inertia-vue3';
-import Navbar from "@/Pages/Navbar";
+import Navbar from '@/Pages/Navbar';
 import Jumbo from '@/Pages/Jumbo';
+import Services from '@/Pages/Services';
 
 export default defineComponent({
     components: {
         Head,
         Link,
         Navbar,
-        Jumbo
+        Jumbo,
+        Services
     },
 })
 </script>
