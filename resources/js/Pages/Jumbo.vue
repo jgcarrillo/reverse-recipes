de23eb042ae907756c1de40c381d33db62323003
Jorge G <jorgegomezweb@gmail.com>
2022-04-11 13:52:04 +0200

    feat: adding jumbo component

---
+++ resources/js/Pages/Jumbo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="text-center text-gray-800 bg-gray-50 p-10 md:py-14">
+        <h1 class="font-lora text-7xl font-bold mt-0 mb-6">Recipes</h1>
+        <h3 class="font-lora text-4xl font-bold mb-8">Explore among more than a thousand recipes and get the one you want</h3>
+        <a
+            class="inline-block px-6 py-2.5 bg-yellow-400 hover:bg-yellow-300 font-bold uppercase rounded shadow-md hover:shadow-lg active:shadow-lg transition duration-500"
+            href="#"
+        >Explore</a
+        >
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
+
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,6 +2,7 @@
     <Head title="Welcome" />
 
     <Navbar />
+    <Jumbo />
 
 </template>
 
@@ -12,12 +13,14 @@
 import { defineComponent } from 'vue'
 import { Head, Link } from '@inertiajs/inertia-vue3';
 import Navbar from "@/Pages/Navbar";
+import Jumbo from '@/Pages/Jumbo';
 
 export default defineComponent({
     components: {
         Head,
         Link,
         Navbar,
+        Jumbo
     },
 })
 </script>
