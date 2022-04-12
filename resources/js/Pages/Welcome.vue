53e8a432cbffa882030ba288ef5921aa681d45e1
Jorge G <jorgegomezweb@gmail.com>
2022-04-12 10:35:54 +0200

    fix: refactor components name fix #2

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,12 +1,11 @@
 <template>
     <Head title="Reverse recipes" />
 
-    <Navbar />
-    <Jumbo />
-    <Services />
-    <Contact />
-    <Footer />
-
+    <the-navbar></the-navbar>
+    <the-jumbo></the-jumbo>
+    <the-services></the-services>
+    <the-contact></the-contact>
+    <the-footer></the-footer>
 </template>
 
 <style>
@@ -15,21 +14,21 @@
 <script>
 import { defineComponent } from 'vue'
 import { Head, Link } from '@inertiajs/inertia-vue3';
-import Navbar from '@/Pages/Navbar';
-import Jumbo from '@/Pages/Jumbo';
-import Services from '@/Pages/Services';
-import Contact from '@/Pages/Contact';
-import Footer from '@/Pages/Footer';
+import TheNavbar from '@/Pages/TheNavbar';
+import TheJumbo from '@/Pages/TheJumbo';
+import TheServices from '@/Pages/TheServices';
+import TheContact from '@/Pages/TheContact';
+import TheFooter from '@/Pages/TheFooter';
 
 export default defineComponent({
     components: {
         Head,
         Link,
-        Navbar,
-        Jumbo,
-        Services,
-        Contact,
-        Footer
+        TheNavbar,
+        TheJumbo,
+        TheServices,
+        TheContact,
+        TheFooter
     },
 })
 </script>
