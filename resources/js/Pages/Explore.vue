bf2a22b7ce38d478bcfb143e93a8407af344b78a
Jorge G <jorgegomezweb@gmail.com>
2022-04-13 11:22:10 +0200

    fix: moving search component

--- resources/js/Pages/Explore.vue
+++ resources/js/Pages/Explore.vue
@@ -8,9 +8,33 @@
         <h3 class="font-lora text-4xl font-bold mb-8">Type something and press <span class="italic">Enter</span></h3>
     </div>
 
+    <!-- Search -->
+    <div class="flex text-sm md:text-base mx-10 md:mx-36 mt-5 mb-9 font-monse">
+        <div class="relative bg-gray-300 p-2 rounded-full border w-full">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 absolute left-0 ml-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+            >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <input
+                v-model="text"
+                class="ml-8 placeholder-gray-500 font-bold bg-transparent w-full focus:outline-none"
+                type="text"
+                placeholder="Food, drinks, recipes, etc."
+            />
+            <button class="absolute right-0 top-2 mr-4 text-gray-500 focus:outline-none" @click="clear">Clear</button>
+        </div>
+    </div>
+    <!-- Search -->
+
     <base-three-grid>
         <template #header>
-            See these examples
+            Look at these examples
         </template>
         <template #default>
             <base-recipe-card></base-recipe-card>
@@ -58,30 +82,6 @@
             </base-recipe-card>
         </template>
     </base-three-grid>
-
-    <!-- Search -->
-    <div class="flex text-sm md:text-base mx-10 md:mx-36 my-5 font-monse">
-        <div class="relative bg-gray-300 p-2 rounded-full border w-full">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 absolute left-0 ml-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-            >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
-            <input
-                v-model="text"
-                class="ml-8 placeholder-gray-500 font-bold bg-transparent w-full focus:outline-none"
-                type="text"
-                placeholder="Food, drinks, recipes, etc."
-            />
-            <button class="absolute right-0 top-2 mr-4 text-gray-500 focus:outline-none" @click="clear">Clear</button>
-        </div>
-    </div>
-    <!-- Search -->
 
     <the-footer></the-footer>
 </template>
